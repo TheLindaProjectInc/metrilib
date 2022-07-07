2e5f242fe98d37eb397f8226ae268981ad99d098
SeqSEE <sarah@sqdmc.net>
2022-07-07 12:21:13 -0400

    add tests, update readme, fix wrong abi in MRC721, add workflow and dependabot

---
+++ src/metriverse/MRC/MRC721.spec.ts
@@ -0,0 +1,43 @@
+import { equal } from 'assert';
+import { ethers } from 'ethers';
+import { MRC721 } from '../';
+import { APIProvider } from '../../provider';
+
+describe('MRC721 tests', () => {
+  const network = 'MainNet';
+  const provider = new APIProvider(network);
+
+  const tokenAddress = '848b9ec4299dac31d21c5db87add813d0cfc58c5'; // PyroPets mainnet address
+
+  const token = new MRC721(tokenAddress, provider);
+
+  const tokenId = `0x${BigInt(0).toString(16)}`;
+
+  it('should match "PyroPets" for the name', async () => {
+    const name = await token.name();
+    equal(name, 'PyroPets');
+  }).timeout(10000);
+
+  it('should match "PYRO" for the symbol', async () => {
+    const symbol = await token.symbol();
+    equal(symbol, 'PYRO');
+  }).timeout(10000);
+
+  it('should match "848b9ec4299dac31d21c5db87add813d0cfc58c5" for the owner of token 0', async () => {
+    const owner = await token.ownerOf(tokenId);
+    equal(
+      owner.toLowerCase().replace('0x', ''),
+      '848b9ec4299dac31d21c5db87add813d0cfc58c5'
+    );
+  }).timeout(10000);
+
+  it('should match "https://pyropets.org/api/metadata/MainNet/0" for the tokenURI of token 0', async () => {
+    const uri = await token.tokenURI(tokenId);
+    equal(uri, 'https://pyropets.org/api/metadata/MainNet/0');
+  }).timeout(10000);
+
+  it('should return address 0x0 (nobody) as approved token 0', async () => {
+    const approved = await token.getApproved(tokenId);
+    equal(approved, ethers.constants.AddressZero);
+  }).timeout(10000);
+});
