e7258c92adbae27bd44981be9f8d63fd4e6770ee
SeqSEE <SeqSEE@users.noreply.github.com>
2023-01-26 23:34:22 -0500

    update contracts

--- src/network/TestNet.ts
+++ src/network/TestNet.ts
@@ -1,8 +1,8 @@
 import Deployment from '../interfaces/Deployment';
 
 export const contracts: Deployment = {
-  Migrations: '0f21d8f6737b90fdd4f14ec6201a1f9d92ec41c2',
-  MetriverseCore: '1f76800c9cbe468bb1008c6108075a117316d700',
-  BurnableMRC721Factory: 'ccc68ad39bdb7be5d09770fb536e0940ac33cd28',
-  MRC721Factory: '641ee85f908955127e44514787758e1238ae8747'
+  Migrations: 'bc119c1295c21098caa98f34a24ef148af6e7047',
+  MetriverseCore: '558f4749ebe0b83b6fe13857a990d6c17de3f9d3',
+  BurnableMRC721Factory: 'd9a80039d3a1720d7035ef8846b79400fe701804',
+  MRC721Factory: '77efeb20067141daf2d58161cac598f51da8e2ab'
 };
