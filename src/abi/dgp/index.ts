1683f28ecca8240deeb457ce1aa058f442e7dfa3
SeqSEE <SeqSEE@users.noreply.github.com>
2023-04-30 15:09:51 -0400

    update docs

--- src/abi/dgp/index.ts
+++ src/abi/dgp/index.ts
@@ -1,5 +1,5 @@
-import {Budget} from './Budget'
-import {DGP} from './DGP'
+import { Budget } from './Budget';
+import { DGP } from './DGP';
 import { Governance } from './Governance';
 
 const ABI = {
