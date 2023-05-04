e2c8c7fe99d3aa849e60cd8f810ab2d96b6c59f5
SeqSEE <SeqSEE@users.noreply.github.com>
2023-05-04 01:32:39 -0400

    add 0.0.4 version of TestNet contracts

--- src/network/TestNet/0.0.4.ts
+++ src/network/TestNet/0.0.4.ts
@@ -1,8 +1,8 @@
 import Deployment from '../../interfaces/Deployment';
 
 export const contracts: Deployment = {
-  Migrations: '0000000000000000000000000000000000000000',
-  MetriverseCore: '0000000000000000000000000000000000000000',
-  BurnableMRC721Factory: '0000000000000000000000000000000000000000',
-  MRC721Factory: '0000000000000000000000000000000000000000'
+  Migrations: 'f45875ade7c3c40090e659961ee4dfee7ffabfc3',
+  MetriverseCore: '91cda2ca5694fbb2fdfc82327938b43f5ed18358',
+  BurnableMRC721Factory: '9759087469405c955c378b649673734ea05f4e11',
+  MRC721Factory: '2df796cdf58ce11917a88f721f6a5ef5ae9b3b8a'
 };
