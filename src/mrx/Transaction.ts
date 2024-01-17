06316fe00a841e720810c3daa430aa3fd7ef7b86
SeqSEE <SeqSEE@users.noreply.github.com>
2024-01-17 10:46:12 -0500

    add optional error to Transaction

--- src/mrx/Transaction.ts
+++ src/mrx/Transaction.ts
@@ -3,4 +3,5 @@ import TransactionReceipt from './TransactionReceipt';
 export interface Transaction {
   txid: string;
   getReceipts: Promise<TransactionReceipt[]>;
+  error?: { message: string; code?: string | number };
 }
